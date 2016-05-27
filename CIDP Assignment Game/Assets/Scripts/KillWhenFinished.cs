8ccac64f2285299e804e539b7be5363b0ab4bd69
andrefarrugia96 <andrefarrugia96@gmail.com>
2016-05-27 18:36:14 +0200

    Build settings
    Build settings set

---
+++ CIDP Assignment Game/Assets/Scripts/KillWhenFinished.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillWhenFinished : MonoBehaviour {
+
+	void Start () {
+		Invoke ("Kill", 2f);
+	}
+
+	
+	void Kill () {
+		Destroy (gameObject);
+	}
+}
